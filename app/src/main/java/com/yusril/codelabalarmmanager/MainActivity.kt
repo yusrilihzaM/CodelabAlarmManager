90f0ee67c50616fe1e10521280bcd02b70dbe26e
yusrilihzaM <mohyusrilihzamaulana@gmail.com>
2021-03-24 17:38:25 +0700

    Cancel Alarm Manager

--- app/src/main/java/com/yusril/codelabalarmmanager/MainActivity.kt
+++ app/src/main/java/com/yusril/codelabalarmmanager/MainActivity.kt
@@ -31,6 +31,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
         // Listener repeating alarm
         binding?.btnRepeatingTime?.setOnClickListener(this)
         binding?.btnSetRepeatingAlarm?.setOnClickListener(this)
+
+        binding?.btnCancelRepeatingAlarm?.setOnClickListener(this)
         // alaram reciver
         alarmReceiver= AlarmReceiver()
     }
@@ -65,6 +67,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
                     val repeatMessage = binding?.edtRepeatingMessage?.text.toString()
                     alarmReceiver.setRepeatingAlarm(this, AlarmReceiver.TYPE_REPEATING,
                         repeatTime, repeatMessage)
+                }
+                R.id.btn_cancel_repeating_alarm ->{
+                    alarmReceiver.cancelAlarm(this,AlarmReceiver.TYPE_REPEATING)
                 }
             }
         }
